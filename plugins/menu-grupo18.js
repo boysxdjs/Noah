b489ce8d72b1d5f2290c937ef3025385a349df01
VersionGrupo3 <154380259+VersionGrupo3@users.noreply.github.com>
2023-12-31 10:00:00 +0100

    🇪🇬 𝐁𝐥𝐨𝐝𝐢𝐞 𝐁𝐨𝐭 🇪🇬

--- plugins/menu-grupo18.js
+++ plugins/menu-grupo18.js
@@ -18,43 +18,43 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
-    const str = ` ? ????????? ??? ?
+    const str = ` 🇪🇬 𝐁𝐥𝐨𝐝𝐢𝐞 𝐁𝐨𝐭 🇪🇬
     
-? *Hola, ${taguser} Este Es El Menu De Grupos 🕷*
+*Hola, ${taguser} Este Es El Menu De Grupos 🇪🇬*
 
-�������T�T�T�T�T?? ? ??�T�T�T�T�T������
- ?? _${usedPrefix}Comunidad *<Invocar>*_
- ?? _${usedPrefix}Sorteos *<Invocar>*_
- ?? _${usedPrefix}Dinamicas *<Invocar>*_
- ?? _${usedPrefix}Vs *<Invocar>*_
- ?? _${usedPrefix}add *num>*_
- ?? _${usedPrefix}kick *<@tag>*_
- ?? _${usedPrefix}kick2 *<@tag>*_
- ?? _${usedPrefix}listanum *<txt>*_
- ?? _${usedPrefix}kicknum *<txt>*_
- ?? _${usedPrefix}grupo *<abrir / cerrar>*_
- ?? _${usedPrefix}grouptime *<opcion> <tiempo>*_
- ?? _${usedPrefix}promote *<@tag>*_
- ?? _${usedPrefix}demote *<@tag>*_
- ?? _${usedPrefix}infogroup_
- ?? _${usedPrefix}resetlink_
- ?? _${usedPrefix}link_
- ?? _${usedPrefix}setname *<txt>*_
- ?? _${usedPrefix}setdesc *<txt>*_
- ?? _${usedPrefix}todos *<Invocar>*_
- ?? _${usedPrefix}setwelcome *<txt>*_
- ?? _${usedPrefix}setbye *<txt>*_
- ?? _${usedPrefix}hidetag *<txt>*_
- ?? _${usedPrefix}hidetag *<audio>*_
- ?? _${usedPrefix}hidetag *<video>*_
- ?? _${usedPrefix}hidetag *<img>*_
- ?? _${usedPrefix}warn *<@tag>*_
- ?? _${usedPrefix}unwarn *<@tag>*_
- ?? _${usedPrefix}listwarn_
- ?? _${usedPrefix}fantasmas_
- ?? _${usedPrefix}setpp *<img>*_
- ?? _admins *<txt>*_ (sin prefijo)
-�ǩ����T�T�T�T�T?? ? ??�T�T�T�T�T������``.trim();
+
+ 🇪🇬 _${usedPrefix}Comunidad *<Invocar>*_
+ 🇪🇬 _${usedPrefix}Sorteos *<Invocar>*_
+ 🇪🇬 _${usedPrefix}Dinamicas *<Invocar>*_
+ 🇪🇬 _${usedPrefix}Vs *<Invocar>*_
+ 🇪🇬 _${usedPrefix}add *num>*_
+ 🇪🇬 _${usedPrefix}kick *<@tag>*_
+ 🇪🇬 _${usedPrefix}kick2 *<@tag>*_
+ 🇪🇬 _${usedPrefix}listanum *<txt>*_
+ 🇪🇬 _${usedPrefix}kicknum *<txt>*_
+ 🇪🇬 _${usedPrefix}grupo *<abrir / cerrar>*_
+ 🇪🇬 _${usedPrefix}grouptime *<opcion> <tiempo>*_
+ 🇪🇬 _${usedPrefix}promote *<@tag>*_
+ 🇪🇬 _${usedPrefix}demote *<@tag>*_
+ 🇪🇬 _${usedPrefix}infogroup_
+ 🇪🇬 _${usedPrefix}resetlink_
+ 🇪🇬 _${usedPrefix}link_
+ 🇪🇬 _${usedPrefix}setname *<txt>*_
+ 🇪🇬 _${usedPrefix}setdesc *<txt>*_
+ 🇪🇬 _${usedPrefix}todos *<Invocar>*_
+ 🇪🇬 _${usedPrefix}setwelcome *<txt>*_
+ 🇪🇬 _${usedPrefix}setbye *<txt>*_
+ 🇪🇬 _${usedPrefix}hidetag *<txt>*_
+ 🇪🇬 _${usedPrefix}hidetag *<audio>*_
+ 🇪🇬 _${usedPrefix}hidetag *<video>*_
+ 🇪🇬 _${usedPrefix}hidetag *<img>*_
+ 🇪🇬 _${usedPrefix}warn *<@tag>*_
+ 🇪🇬 _${usedPrefix}unwarn *<@tag>*_
+ 🇪🇬 _${usedPrefix}listwarn_
+ 🇪🇬 _${usedPrefix}fantasmas_
+ 🇪🇬 _${usedPrefix}setpp *<img>*_
+ 🇪🇬 _admins *<txt>*_ (sin prefijo)
+`.trim();
     if (m.isGroup) {
       // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
@@ -65,7 +65,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
       conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
     }
   } catch {
-    conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙼𝙴𝙽𝚄 𝚃𝙸𝙴𝙽𝙴 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁 𝚈 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙴𝙽𝚅𝙸𝙰𝚁𝙻𝙾, 𝚁𝙴𝙿𝙾𝚁𝚃𝙴𝙻𝙾 𝙰𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃*', m);
+    conn.reply(m.chat, '*[â—ðˆðð…ðŽâ—] ð™´ð™» ð™¼ð™´ð™½ðš„ ðšƒð™¸ð™´ð™½ð™´ ðš„ð™½ ð™´ðšðšð™¾ðš ðšˆ ð™½ð™¾ ð™µðš„ð™´ ð™¿ð™¾ðš‚ð™¸ð™±ð™»ð™´ ð™´ð™½ðš…ð™¸ð™°ðšð™»ð™¾, ðšð™´ð™¿ð™¾ðšðšƒð™´ð™»ð™¾ ð™°ð™» ð™¿ðšð™¾ð™¿ð™¸ð™´ðšƒð™°ðšð™¸ð™¾ ð™³ð™´ð™» ð™±ð™¾ðšƒ*', m);
   }
 };
 handler.command = /^(grupos1|Grupos1)$/i;
