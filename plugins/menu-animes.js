96a62dbef2642a42e5a0fb70d78a9e9f4483aa60
xxddbotsx <149536321+boysxdjs@users.noreply.github.com>
2024-01-02 04:47:53 +0100

    🫧 𝗬𝘅𝗺𝗶𝘀 𝗕𝗼𝘁 🫧

--- plugins/menu-animes.js
+++ plugins/menu-animes.js
@@ -18,29 +18,29 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
-    const str = ` 🇪🇬️ 𝐁𝐥𝐨𝐝𝐢𝐞 𝐁𝐨𝐭 🇪🇬️
+    const str = `  🫧 𝗬𝘅𝗺𝗶𝘀 𝗕𝗼𝘁 🫧
     
-➤ *Hola, ${taguser} Estos Comandos Solo Pueden Ser Usado Por Admins E Incluso El Bot Debe Ser Admin 🇪🇬️*
+➤ *Hola, ${taguser} Estos Comandos Solo Pueden Ser Usado Por Admins E Incluso El Bot Debe Ser Admin 🫧️*
 
  ◤━━━━━━━━━━━◥
- ►🇪🇬️ _${usedPrefix}LigaMixta5_
- ►🇪🇬️ _${usedPrefix}Liga5_
- ►🇪🇬️ _${usedPrefix}LigaMixta_
- ►🇪🇬️ _${usedPrefix}Liga_
- ►🇪🇬️ _${usedPrefix}ScrimMixta_
- ►🇪🇬️ _${usedPrefix}Scrim_
- ►🇪🇬️ _${usedPrefix}Masc4_
- ►🇪🇬️ _${usedPrefix}Masc8_
- ►🇪🇬️ _${usedPrefix}Masc12_
- ►🇪🇬️ _${usedPrefix}Masc16_
- ►🇪🇬️ _${usedPrefix}Masc20_
- ►🇪🇬️ _${usedPrefix}Masc24_
- ►🇪🇬️ _${usedPrefix}Fem4_
- ►🇪🇬️ _${usedPrefix}Fem8_
- ►🇪🇬️ _${usedPrefix}Fem12_
- ►🇪🇬️ _${usedPrefix}Fem16_
- ►🇪🇬️ _${usedPrefix}Fem20_
- ►🇪🇬️ _${usedPrefix}Fem24_
+ 🫧››  _${usedPrefix}LigaMixta5_
+ 🫧››  _${usedPrefix}Liga5_
+ 🫧››  _${usedPrefix}LigaMixta_
+ 🫧››  _${usedPrefix}Liga_
+ 🫧››  _${usedPrefix}ScrimMixta_
+ 🫧››  _${usedPrefix}Scrim_
+ 🫧››  _${usedPrefix}Masc4_
+ 🫧››  _${usedPrefix}Masc8_
+ 🫧››  _${usedPrefix}Masc12_
+ 🫧››  _${usedPrefix}Masc16_
+ 🫧››  _${usedPrefix}Masc20_
+ 🫧››  _${usedPrefix}Masc24_
+ 🫧››  _${usedPrefix}Fem4_
+ 🫧››  _${usedPrefix}Fem8_
+ 🫧››  _${usedPrefix}Fem12_
+ 🫧››  _${usedPrefix}Fem16_
+ 🫧››  _${usedPrefix}Fem20_
+ 🫧››  _${usedPrefix}Fem24_
  ◣━━━━━━━━━━━◢`.trim();
     if (m.isGroup) {
       // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
